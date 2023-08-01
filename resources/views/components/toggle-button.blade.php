af2c70a47fe64b08a8469937057d85fa53c8e6f1
wallo <andrewdwallo@gmail.com>
2023-08-01 00:18:17 -0400

    v3 compatible release

--- resources/views/components/toggle-button.blade.php
+++ resources/views/components/toggle-button.blade.php
@@ -1,76 +1,102 @@
-<x-dynamic-component
-    :component="$getFieldWrapperView()"
-    :field="$field"
-    :id="$getId()"
-    :label="$getLabel()"
-    :label-sr-only="$isLabelHidden()"
-    :helper-text="$getHelperText()"
-    :hint="$getHint()"
-    :hint-color="$getHintColor()"
-    :hint-icon="$getHintIcon()"
-    :required="$isRequired()"
-    :state-path="$getStatePath()"
->
+<x-dynamic-component :component="$getFieldWrapperView()" :field="$field">
+    @php
+        $id = $getId();
+        $statePath = $getStatePath();
+        $isDisabled = $isDisabled();
+        $offLabel = $getOffLabel();
+        $onLabel = $getOnLabel();
+        $offColor = $getOffColor() ?? 'gray';
+        $onColor = $getOnColor() ?? 'primary';
+    @endphp
+
     <div
-        class="flex items-center mt-1"
+        class="flex items-center"
         x-data="{
-            state: $wire.{{ $applyStateBindingModifiers('entangle(\'' . $getStatePath() . '\')') }},
+                state: $wire.{{ $applyStateBindingModifiers("entangle('{$statePath}')") }}
         }"
     >
+        <style>
+            .filament-selectify-toggle-button-on {
+                border-top-left-radius: 0.5rem;
+                border-bottom-left-radius: 0.5rem;
+            }
+
+            .filament-selectify-toggle-button-off {
+                border-top-right-radius: 0.5rem;
+                border-bottom-right-radius: 0.5rem;
+            }
+        </style>
         <label
-            for="{{ $getId() }}-{{ $getOnLabel() }}"
+            for="{{ $id }}-{{ $onLabel }}"
             x-on:click="state = true"
-            x-bind:class="{
-                '{{
-                    match ($getOnColor()) {
-                        'danger' => 'bg-danger-600',
-                        'primary' => 'bg-primary-600',
-                        'secondary' => 'bg-gray-500',
-                        'warning' => 'bg-warning-600',
-                        default => 'bg-success-600',
-                    }
-                }} text-white': state,
-                'bg-gray-200 dark:bg-white/10 text-gray-900 dark:text-white': !state,
-            }"
+            x-bind:class="
+                state
+                    ? '{{
+                        match ($onColor) {
+                            'gray' => 'bg-white text-gray-950 hover:bg-gray-50 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 ring-1 ring-gray-950/10 dark:ring-white/20',
+                            default => 'bg-custom-600 text-white hover:bg-custom-500 dark:bg-custom-500 dark:hover:bg-custom-400 focus:ring-custom-500/50 dark:focus:ring-custom-400/50',
+                        }
+                      }}'
+                    : 'bg-gray-200 dark:bg-white/10 text-gray-900 dark:text-white'
+            "
+            x-bind:style="
+                state
+                    ? '{{ \Filament\Support\get_color_css_variables($onColor, shades: [600, 500, 400]) }}'
+                    : null
+            "
             {{
                 $attributes
-                ->merge($getExtraAttributes())
+                ->merge($getExtraAttributes(), escape: false)
+                ->merge($getExtraAlpineAttributes(), escape: false)
                 ->class([
-                    'filament-companies-toggle-button-on relative inline-block h-10 rounded-l-lg pt-2 pb-2 pl-3 pr-3 text-sm text-center transition-all cursor-pointer',
+                    'filament-selectify-toggle-button-on relative inline-block h-10 py-2 px-3 text-sm text-center transition-all cursor-pointer',
                 ])
             }}
-            {{ $getExtraAlpineAttributeBag() }}
         >
-            {{ $getOnLabel() }}
-            <input type="radio" x-model="state" class="absolute left-0 opacity-0" />
+            {{ $onLabel }}
+            <input
+                type="radio"
+                x-model="state"
+                class="absolute left-0 opacity-0"
+            />
         </label>
 
         <label
-            for="{{ $getId() }}-{{ $getOffLabel() }}"
+            for="{{ $id }}-{{ $offLabel }}"
             x-on:click="state = false"
-            x-bind:class="{
-                '{{
-                    match ($getOffColor()) {
-                        'primary' => 'bg-primary-600',
-                        'secondary' => 'bg-gray-500',
-                        'success' => 'bg-success-600',
-                        'warning' => 'bg-warning-600',
-                        default => 'bg-danger-600',
-                    }
-                }} text-white': !state,
-                'bg-gray-200 dark:bg-white/10 text-gray-900 dark:text-white': state,
-            }"
+            x-bind:class="
+                !state
+                    ? '{{
+                        match ($offColor) {
+                            'gray' => 'bg-white text-gray-950 hover:bg-gray-50 dark:bg-white/5 dark:text-white dark:hover:bg-white/10 ring-1 ring-gray-950/10 dark:ring-white/20',
+                            default => 'bg-custom-600 text-white hover:bg-custom-500 dark:bg-custom-500 dark:hover:bg-custom-400 focus:ring-custom-500/50 dark:focus:ring-custom-400/50',
+                        }
+                      }}'
+                    : 'bg-gray-200 dark:bg-white/10 text-gray-900 dark:text-white'
+            "
+            x-bind:style="
+                !state
+                    ? '{{ \Filament\Support\get_color_css_variables($offColor, shades: [600, 500, 400]) }}'
+                    : null
+            "
             {{
                 $attributes
                 ->merge($getExtraAttributes())
                 ->class([
-                    'filament-companies-toggle-button-off relative inline-block h-10 rounded-r-lg pt-2 pb-2 pl-3 pr-3 text-sm text-center transition-all cursor-pointer',
+                    'filament-selectify-toggle-button-off relative inline-block h-10 py-2 px-3 text-sm text-center transition-all cursor-pointer',
                 ])
             }}
             {{ $getExtraAlpineAttributeBag() }}
         >
-            {{ $getOffLabel() }}
-            <input type="radio" x-model="!state" class="absolute left-0 opacity-0" />
+            {{ $offLabel }}
+            <input
+                type="radio"
+                x-model="!state"
+                class="absolute left-0 opacity-0"
+            />
         </label>
     </div>
 </x-dynamic-component>
+
+
+
